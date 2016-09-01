2d31d7aa60d0abf7047e03b681b029b819b46e02
Kimdeokho <ejzh57@gmail.com>
2016-09-01 18:43:50 +0900

    툴 프로젝트 추가

---
+++ mystarcraft/Tool/ToolView.cpp
@@ -0,0 +1,110 @@
+
+// ToolView.cpp : CToolView 클래스의 구현
+//
+
+#include "stdafx.h"
+#include "Tool.h"
+
+#include "ToolDoc.h"
+#include "ToolView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CToolView
+
+IMPLEMENT_DYNCREATE(CToolView, CScrollView)
+
+BEGIN_MESSAGE_MAP(CToolView, CScrollView)
+	// 표준 인쇄 명령입니다.
+	ON_COMMAND(ID_FILE_PRINT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_DIRECT, &CScrollView::OnFilePrint)
+	ON_COMMAND(ID_FILE_PRINT_PREVIEW, &CScrollView::OnFilePrintPreview)
+END_MESSAGE_MAP()
+
+// CToolView 생성/소멸
+
+CToolView::CToolView()
+{
+	// TODO: 여기에 생성 코드를 추가합니다.
+
+}
+
+CToolView::~CToolView()
+{
+}
+
+BOOL CToolView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: CREATESTRUCT cs를 수정하여 여기에서
+	//  Window 클래스 또는 스타일을 수정합니다.
+
+	return CScrollView::PreCreateWindow(cs);
+}
+
+// CToolView 그리기
+
+void CToolView::OnDraw(CDC* /*pDC*/)
+{
+	CToolDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 여기에 원시 데이터에 대한 그리기 코드를 추가합니다.
+}
+
+void CToolView::OnInitialUpdate()
+{
+	CScrollView::OnInitialUpdate();
+
+	CSize sizeTotal;
+	// TODO: 이 뷰의 전체 크기를 계산합니다.
+	sizeTotal.cx = sizeTotal.cy = 100;
+	SetScrollSizes(MM_TEXT, sizeTotal);
+}
+
+
+// CToolView 인쇄
+
+BOOL CToolView::OnPreparePrinting(CPrintInfo* pInfo)
+{
+	// 기본적인 준비
+	return DoPreparePrinting(pInfo);
+}
+
+void CToolView::OnBeginPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄하기 전에 추가 초기화 작업을 추가합니다.
+}
+
+void CToolView::OnEndPrinting(CDC* /*pDC*/, CPrintInfo* /*pInfo*/)
+{
+	// TODO: 인쇄 후 정리 작업을 추가합니다.
+}
+
+
+// CToolView 진단
+
+#ifdef _DEBUG
+void CToolView::AssertValid() const
+{
+	CScrollView::AssertValid();
+}
+
+void CToolView::Dump(CDumpContext& dc) const
+{
+	CScrollView::Dump(dc);
+}
+
+CToolDoc* CToolView::GetDocument() const // 디버그되지 않은 버전은 인라인으로 지정됩니다.
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CToolDoc)));
+	return (CToolDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CToolView 메시지 처리기
